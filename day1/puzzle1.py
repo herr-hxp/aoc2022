8f076babc5f4362b51c34424f1ed1b33f9c74937
herr-hxp <haxxpet@gmail.com>
2022-12-01 13:25:04 +0100

    new solution with the correct input data

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,32 +1,23 @@
-# debug flag
-DEBUG=0
+with open('input', 'r') as file:
+    # put the lines in a list, and strip the pesky newlines
+    elflist = [x.rstrip() for x in file]
+    # create a sum var and a sum list to put the sums in
+    sum = 0
+    sumlist = []
+    # go through our list and check if the line is an integer, then add it to the sum,
+    # if not, append the sum to the list of sums and start summing up the next part
+    for line in elflist:
+        if line.isdigit():
+            sum = sum+int(line)
+        else:
+            sumlist.append(sum)
+            sum = 0
 
-# our five elfs
-elf_one = [1000, 2000, 3000]
-elf_two = [4000]
-elf_three = [5000, 6000]
-elf_four = [7000, 8000, 9000]
-elf_five = [10000]
+# set the first item in the list to the largest number and compare the rest, if it's bigger we make the variable reflect the new number
+max = sumlist[0]
+for num in sumlist:
+    if num > max:
+        max = num
 
-# elf baked into one list
-elf_list = [elf_one, elf_two, elf_three, elf_four, elf_five]
-
-# debug function
-def debug(str):
-    if DEBUG:
-        print(str)
-
-def endmsg(num):
-    print("The elf with the most calories is carrying:",num)
-
-# add together each elf's total calories
-res = list(map(sum, elf_list))
-debug(res)
-
-# find the largest number in the result above
-max = res[0]
-for i in range(0,len(res)):
-    if res[i] > max:
-        max = res[i]
-
-endmsg(max)
+# print out the biggest number we could find in the list
+print("The elf carrying the most has a total of:",max)
